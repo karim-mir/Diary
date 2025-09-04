66781d13dce42a967f9e476327e9fb07bdc533dc
NazKar@Nazir <karimov.nazir00@yandex.ru>
2025-09-04 20:17:26 +0500

    создал модель и представления для Django-приложения Diary

--- diary/admin.py
+++ diary/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Entry
 
-# Register your models here.
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'created_at')
+    search_fields = ('title', 'content')
--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Entry
+
+class EntryListView(LoginRequiredMixin, ListView):
+    model = Entry
+    context_object_name = 'entries'
+    template_name = 'diary/entry_list.html'
+
+    def get_queryset(self):
+        return Entry.objects.filter(user=self.request.user).order_by('-created_at')
 
 
+class EntryDetailView(LoginRequiredMixin, DetailView):
+    model = Entry
+    context_object_name = 'entry'
+    template_name = 'diary/entry_detail.html'
+
+    def get_queryset(self):
+        return Entry.objects.filter(user=self.request.user)
+
+
+class EntryCreateView(LoginRequiredMixin, CreateView):
+    model = Entry
+    fields = ['title', 'content']
+    template_name = 'diary/entry_form.html'
+    success_url = reverse_lazy('diary:entry_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class EntryUpdateView(LoginRequiredMixin, UpdateView):
+    model = Entry
+    fields = ['title', 'content']
+    template_name = 'diary/entry_form.html'
+    success_url = reverse_lazy('diary:entry_list')
+
+    def get_queryset(self):
+        return Entry.objects.filter(user=self.request.user)
+
+
+class EntryDeleteView(LoginRequiredMixin, DeleteView):
+    model = Entry
+    template_name = 'diary/entry_confirm_delete.html'
+    success_url = reverse_lazy('diary:entry_list')
+
+    def get_queryset(self):
+        return Entry.objects.filter(user=self.request.user)
